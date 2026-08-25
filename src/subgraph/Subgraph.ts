91dcc824b801f8cbbba7cfda29d4576a78c1d12d
Patrick Murphy <git@syzgyn.com>
2026-08-25 16:59:56 -0400

    Import PR 4911: fix subgraph reroute serialization.
    Include reroutes in Subgraph.asSerialisable when the subgraph has any.

--- src/subgraph/Subgraph.ts
+++ src/subgraph/Subgraph.ts
@@ -374,6 +374,9 @@ export class Subgraph extends LGraph implements BaseLGraph, Serialisable<Exporte
       nodes: this.nodes.map(node => node.serialize()),
       groups: this.groups.map(group => group.serialize()),
       links: [...this.links.values()].map(x => x.asSerialisable()),
+      reroutes: this.reroutes.size
+        ? [...this.reroutes.values()].map(x => x.asSerialisable())
+        : undefined,
       extra: this.extra,
     }
   }
