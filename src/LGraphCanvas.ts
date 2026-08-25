46cb5a8c93892caf373c8bfe72a302b6d6d9a72b
Patrick Murphy <git@syzgyn.com>
2026-08-25 17:32:21 -0400

    Import PR 4928: middle-button link delete on reroutes.
    Resolve the link id from reroute linkIds when deleting a link segment that
    is a Reroute, instead of using the reroute id as a link id.

--- src/LGraphCanvas.ts
+++ src/LGraphCanvas.ts
@@ -6319,9 +6319,17 @@ export class LGraphCanvas implements CustomEventDispatcher<LGraphCanvasEventMap>
         break
       }
 
-      case "Delete":
-        graph.removeLink(segment.id)
+      case "Delete": {
+        // segment can be a Reroute object, in which case segment.id is the reroute id
+        const linkId =
+          segment instanceof Reroute
+            ? segment.linkIds.values().next().value
+            : segment.id
+        if (linkId !== undefined) {
+          graph.removeLink(linkId)
+        }
         break
+      }
       default:
       }
     }
